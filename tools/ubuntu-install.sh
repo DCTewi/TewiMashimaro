c15e45ed637d1e83c6b45029d3a09fbef9dd9045
DCTewi <dctewi@dctewi.com>
2024-10-17 21:46:50 +0800

    [bugfix] 更新caddy安装命令

--- tools/ubuntu-install.sh
+++ tools/ubuntu-install.sh
@@ -65,8 +65,8 @@ echo '| CADDY INSTALLATION |'
 echo '----------------------'
 
 if ! exists caddy; then
-    sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+    sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
     sudo apt update
     sudo apt install caddy -y
@@ -84,8 +84,8 @@ echo '--------------------'
 
 ## final start
 
-sudo systemctl start mashimaro.service
-sudo systemctl start caddy
+sudo systemctl restart mashimaro.service
+sudo systemctl restart caddy
 
 echo '--------------------'
 echo '| INSTALL FINISHED |'
